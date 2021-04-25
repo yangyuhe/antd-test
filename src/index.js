cfa42197335cd7f2517428e15bad3e8447fe249b
hexiang <xhe@hpe.com>
2021-04-25 18:39:49 +0800

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import "./index.css";
-import { Button } from "antd";
 function App() {
   const [count, setCount] = useState(0);
   useEffect(() => {
