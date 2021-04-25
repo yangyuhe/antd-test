a9c19dc226ea65dcd5ad64b2facbd835857b5efb
hexiang <xhe@hpe.com>
2021-04-25 18:33:49 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Button } from "antd";
+function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+  }, []);
+  return (
+    <div>
+      <h1>计数：{count}</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("container"));
